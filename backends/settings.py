287c7b756d63acd0d652c7a037739279ac414054
Rajesh <rjcse131998@gmail.com>
2022-03-02 13:05:37 +0530

    env and gitignore file added

--- backends/settings.py
+++ backends/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,12 +136,12 @@ DJOSER = {
 
 # SMTP setting for sending mails
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = config("EMAIL_BACKEND")
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rajesh.bluethink@gmail.com'
-EMAIL_HOST_PASSWORD = "bwonlasyviydxveg"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 # DEFAULT_FROM_EMAIL = 'Testing celery with mailing service <email here>'
 
 # Internationalization
